8f4e2e99323724eb1421615fdf8093852f222ce6
Vivek003-coder <sharmavivek2303ktr@gmail.com>
2026-04-17 13:24:17 +0530

    intital a

--- PrintingLCS.java
+++ PrintingLCS.java
@@ -16,7 +16,7 @@ class PrintingLCS {
         StringBuilder sb=new StringBuilder();
         int i=n;
         int j=m;
-        while (i>0 || j>0) {
+        while (i>0 && j>0) {
             if(a.charAt(i-1) ==b.charAt(j-1)){
                 sb.append(a.charAt(i-1));
                 i--;
