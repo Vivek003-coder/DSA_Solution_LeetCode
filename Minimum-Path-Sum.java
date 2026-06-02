484df491146f05cf500fe00f1d068de487a9683c
VIVEK KUMAR <sharmavivek2303ktr@gmail.com>
2026-06-02 10:01:20 +0530

    [64] [Time Beats: 78.92%] [Memory Beats: 21.46%] - LeetPush

--- Minimum-Path-Sum.java
+++ Minimum-Path-Sum.java
@@ -2,17 +2,25 @@
 2    public int minPathSum(int[][] grid) {
 3        int n=grid.length;
 4        int m=grid[0].length;
-5        int[][] dp=new int[n][m];
-6        int sum=0;
-7
-8        for(int i=0;i<n;i++){
-9            for(int j=0;j<m;j++){
-10               if(i==0 && j==0 )dp[i][j]=grid[i][j];
-11               else if(i==0) dp[i][j]=grid[i][j]+dp[i][j-1];
-12               else if(j==0) dp[i][j]=grid[i][j]+dp[i-1][j];
-13               else dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
-14            }
-15        }
-16        return dp[n-1][m-1];
-17    }
-18}
+5
+6        int[][] dp=new int[n+1][m+1];
+7        for(int i=0;i<=n;i++){
+8            for(int j=0;j<=m;j++){
+9                dp[i][j]=-1;
+10            }
+11        }
+12
+13        return solve(0,0,n,m,grid,dp);
+14    }
+15    public static int solve(int i,int j,int n,int m,int[][] arr,int[][] dp){
+16        if(i>=n || j>=m)return Integer.MAX_VALUE;
+17        if(i==n-1 && j==m-1) return arr[i][j];
+18        if(dp[i][j]!=-1) return dp[i][j];
+19
+20        int right=solve(i+1,j,n,m,arr,dp);
+21        int down=solve(i,j+1,n,m,arr,dp);
+22
+23        dp[i][j]=arr[i][j]+Math.min(right,down);
+24        return dp[i][j];
+25    }
+26}
